6a217ef2eddffa13d2a2e1228f034e196235a5e9
bipro1992 <biprokvs@gmail.com>
2019-02-22 13:21:38 +0530

    Zuul gatway service

---
+++ Zuul-gateway/src/main/java/zuul/gateway/ZuulGateway.java
@@ -0,0 +1,24 @@
+package zuul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGateway {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(ZuulGateway.class, args);
+
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
